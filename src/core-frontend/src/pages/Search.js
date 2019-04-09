6aa1aa35d6d911fc2c8f8ce429712a6e7851660e
Sadie Au <sadieau@Sadies-MBP.hitronhub.home>
2019-04-09 00:18:31 -0700

    started search page

---
+++ src/core-frontend/src/pages/Search.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      position: "absolute",
+      left: "15%",
+      top: "10%",
+      width: "70%",
+    },
+    dense: {
+      marginTop: 16,
+    },
+    menu: {
+      width: 200,
+    },
+  });
+
+class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+           loggedIn: true,
+           settingsDisplay: false,
+           list: [],
+        }
+
+        this.addSearchItem = this.addSearchItem.bind(this); 
+        this.removeSearchItem = this.removeSearchItem.bind(this);
+     }
+
+
+     handleChange = name => event => {
+        this.setState({
+          [name]: event.target.value,
+        });
+      };
+
+
+    render(){
+
+        const { classes } = this.props;
+
+        return(
+            <div id="searchContainer">
+                <TextField
+                    id="outlined-search"
+                    label="Search for users"
+                    type="search"
+                    className={classes.textField}
+                    margin="normal"
+                    variant="outlined"
+                    />
+
+            </div>
+        );
+    }
+}
+
+Search.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+export default withStyles(styles)(Search); 
